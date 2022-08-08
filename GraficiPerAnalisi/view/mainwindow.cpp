7088412e5dc0f8147f5f22dd3780da0c2677b7b7
EricaCavaliere <erica.cavaliere.euganeoinf@gmail.com>
2022-08-08 18:19:46 +0200

    Impostato la finestra principale

--- GraficiPerAnalisi/view/mainwindow.cpp
+++ GraficiPerAnalisi/view/mainwindow.cpp
@@ -6,7 +6,6 @@ MainWindow::MainWindow(QWidget *parent)
     :QMainWindow(parent),nPunti(10),valoreMax(100),nLinee(2),tabella(generateRandomData(nLinee,valoreMax,nPunti)){
 
     setMinimumSize(500,500);
-
     QMenuBar* menuBar = new QMenuBar(this);
     setMenuBar(menuBar);
 
@@ -21,24 +20,70 @@ MainWindow::MainWindow(QWidget *parent)
     file->addAction(apri);
     file->addAction(salva);
     file->addAction(salvaTutto);
+    file->addSeparator();
     file->addAction(chiudi);
 
+    QMenu* grafico = new QMenu("Grafico",menuBar);
+    QAction* addLinea = new QAction("Aggiungi Linea",grafico);
+    QAction* addPunto = new QAction("Aggiungi punto",grafico);
+    QAction* delLinea = new QAction("Elimina Linea",grafico);
+    QAction* delPunto = new QAction("Elimina punto");
+    menuBar->addMenu(grafico);
+    grafico->addSeparator();
+    grafico->addAction(addLinea);
+    grafico->addAction(addPunto);
+    grafico->addAction(delLinea);
+    grafico->addAction(delPunto);
+
+
     QMenu* finestra = new QMenu("Finestra",menuBar);
-    QAction* colore = new QAction("Colore",finestra);
-    QAction* legenda = new QAction("Legenda",finestra);
+    QAction* destra = new QAction("Legenda a destra",finestra);
+    QAction* sinistra = new QAction("Legenda a sinistra", finestra);
+    QAction* sopra = new QAction("Legenda sopra",finestra);
+    QAction* sotto = new QAction("Legenda sotto",finestra);
     QAction* animazione = new QAction("Animazione",finestra);
+    destra->setCheckable(true);
+    sinistra->setCheckable(true);
+    sopra->setCheckable(true);
+    sotto->setCheckable(true);
+    animazione->setCheckable(true);
     menuBar->addMenu(finestra);
-    finestra->addAction(colore);
-    finestra->addAction(legenda);
+    finestra->addSeparator();
+    finestra->addAction(destra);
+    finestra->addAction(sinistra);
+    finestra->addAction(sopra);
+    finestra->addAction(sotto);
+    finestra->addSeparator();
     finestra->addAction(animazione);
+    QActionGroup* legenda = new QActionGroup(finestra);
+    legenda->addAction(destra);
+    legenda->addAction(sinistra);
+    legenda->addAction(sopra);
+    legenda->addAction(sotto);
+    sopra->setChecked(true);
+
+
+    QMenu* altro = new QMenu("Altro",menuBar);
+    QAction* impostazioni = new QAction("Impostazioni",altro);
+    QAction* info = new QAction("Info",altro);
+    menuBar->addMenu(altro);
+    altro->addAction(impostazioni);
+    altro->addAction(info);
+
 
     QTabWidget *tab = new QTabWidget(this);
     tab->addTab(creaGraficoLinea(),tr("Grafico Linea"));
     tab->addTab(creaGraficoTorta(),tr("Grafico Torta"));
     tab->addTab(creaGraficoPunti(),tr("Grafico Punti"));
+    tab->setMovable(true);
+
+    //istruzioni per chiudere una scheda tab
+    //bisgona vedere come connettere la pagine del grafico con la scheda interessata
+    //tab->setTabsClosable(true);
+    //connect(tab->tabBar(), &QTabBar::tabCloseRequested, tab->tabBar(), &QTabBar::removeTab);
+
 
     setCentralWidget(tab);
-
 }
 
 MainWindow::~MainWindow(){}
@@ -60,6 +105,7 @@ QChartView* MainWindow::creaGraficoLinea()const{
     }
     chart->createDefaultAxes();
     QChartView* view = new QChartView(chart);
+    view->setRenderHints(QPainter::Antialiasing);
     return view;
 }
 
@@ -88,6 +134,7 @@ QChartView* MainWindow::creaGraficoTorta() const
         chart->addSeries(series);
     }
     QChartView* view = new QChartView(chart);
+    view->setRenderHints(QPainter::Antialiasing);
     return view;
 }
 
@@ -108,6 +155,7 @@ QChartView* MainWindow::creaGraficoPunti() const
     }
     chart->createDefaultAxes();
     QChartView* view = new QChartView(chart);
+    view->setRenderHints(QPainter::Antialiasing);
     return view;
 }
 
